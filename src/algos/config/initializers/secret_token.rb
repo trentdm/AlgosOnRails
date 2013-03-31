3b3997247634d600c0dc04771d1fe0a484b5ffcf
Trent Miller <trent@Trents-MacBook-Pro.local>
2013-03-30 18:25:02 -0600

    algos: +created default RoR project

---
+++ src/algos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Algos::Application.config.secret_token = '9aafef8fac8261c69f5b88d7f754c66edb0f0635e764db187b24467d6f5e4ce1f99c8957b8d3ed5d6e3deb5cbe2acf0a167a88cfd9680bb95d45c4ceedb02d2a'
