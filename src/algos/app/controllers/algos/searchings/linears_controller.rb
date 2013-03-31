09af741bde324f07331089b7f1ab67adab745d2b
Trent Miller <trent@Trents-MacBook-Pro.local>
2013-03-31 13:40:09 -0600

    algos: +finished basic template for demoing algorithms

--- src/algos/app/controllers/algos/searchings/linears_controller.rb
+++ src/algos/app/controllers/algos/searchings/linears_controller.rb
@@ -1,5 +1,7 @@
 class Algos::Searchings::LinearsController < ApplicationController
   def index
+    @input = "Sally sells sea shells by the sea shore."
+    @parameter = "se"
     @output = 'hello world' 
   end
 end
